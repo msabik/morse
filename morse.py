815777d306887360bfb957dfae65621ec2d3b070
Martin <ms@efficientip.com>
2019-04-02 23:43:14 +0200

    100% passing tests for one-word Morse decoder

--- morse.py
+++ morse.py
@@ -1,3 +1,38 @@
 class Morse:
-    def decode(self, message):
-        pass # TODO: implement the behavior!
+    class Morse:
+        ALPHABET = {
+            '.-': 'A',
+            '-...': 'B',
+            '-.-.': 'C',
+            '-..': 'D',
+            '.': 'E',
+            '..-.': 'F',
+            '--.': 'G',
+            '....': 'H',
+            '..': 'I',
+            '.---': 'J',
+            '-.-': 'K',
+            '.-..': 'L',
+            '--': 'M',
+            '-.': 'N',
+            '---': 'O',
+            '.--.': 'P',
+            '--.-': 'Q',
+            '.-.': 'R',
+            '...': 'S',
+            '-': 'T',
+            '..-': 'U',
+            '...-': 'V',
+            '.--': 'W',
+            '-..-': 'X',
+            '-.--': 'Y',
+            '--..': 'Z'
+        }
+
+        def decode(self, message):
+            if message == "":
+                return ""
+
+            symbols = message.split(" ")
+            letters = [self.ALPHABET[s] for s in symbols]
+            return ''.join(letters)
